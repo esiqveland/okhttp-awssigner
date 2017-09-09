8e94022b4b63e844179ee70a8c9646ad2eb5b009
Eivind Siqveland Larsen <eivind@siqve.land>
2017-09-09 11:15:10 +0200

    removed dependency on vavr

---
+++ src/main/java/com/github/esiqveland/okhttp3/utils/Tuple.java
@@ -0,0 +1,9 @@
+package com.github.esiqveland.okhttp3.utils;
+
+public class Tuple {
+
+    public static <T1,T2> Tuple2<T1,T2> of(T1 first, T2 second) {
+            return new Tuple2<>(first, second);
+        }
+
+}
