470cb5862173b2bbd40293dede26c8212fcb40b3
Eivind Siqveland <eivind@siqve.land>
2018-03-10 19:14:13 +0100

    java9 for okhttp

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.github.esiqveland.okhttp3.awssigner
 {
     // wait for versions with Automatic-Module-Name set in MANIFEST.MF
     // See: http://blog.joda.org/2017/05/java-se-9-jpms-automatic-modules.html
-    requires okhttp;
+    requires okhttp3;
     requires okio;
 
     requires com.google.common;
